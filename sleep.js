8bd26bef7180b59cc422de94d08b9c4cc3d6e01b
Adeniran Solomon <soluwaseun806@gmail.com>
2023-08-13 03:30:39 +0100

    async created

---
+++ sleep.js
@@ -0,0 +1,14 @@
+//an asynchronous function that sleeps for millis milliseconds
+/**
+ * @param {number} millis
+ */
+ async function sleep(millis) {
+    return new Promise(resolve => {
+    setTimeout(resolve, millis); 
+  });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
